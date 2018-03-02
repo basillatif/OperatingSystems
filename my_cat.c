667edccacb2f6c8dcb8b08951ce031f2774a8872
basillatif <basillatif@gmail.com>
2018-03-02 08:57:13 -0800

    Final

--- my_cat.c
+++ my_cat.c
@@ -2,12 +2,14 @@
 #include <unistd.h>
 
 
-main(int argc, char **argv) {
+int main(int argc, char **argv) {
 int fd, nread;
-char buffer[516];
-fd = open(argv[1], O_RDONLY);
-while ((nread = read(fd, buffer, sizeof(buffer))) > 0){
-  write(1, buffer, nread);
+for(int i = 1; i < argc; i++){
+  char buffer[516];
+  fd = open(argv[i], O_RDONLY);
+  while ((nread = read(fd, buffer, sizeof(buffer))) > 0){
+    write(1, buffer, nread);
+  }
 }
-return 0;
+  return 0;
 }
